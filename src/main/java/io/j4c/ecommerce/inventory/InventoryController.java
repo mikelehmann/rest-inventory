17ea8b402c41b0c4f8775a0144ae9db08e1f4763
mikelehmann <mike.lehmann@gmail.com>
2017-10-10 10:51:25 -0700

    Update InventoryController.java

--- src/main/java/io/j4c/ecommerce/inventory/InventoryController.java
+++ src/main/java/io/j4c/ecommerce/inventory/InventoryController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. Change
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. 
  */
 package io.j4c.ecommerce.inventory;
 
