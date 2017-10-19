c6f7619d1d333d1b2224268356f77cb9b4af8a01
mikelehmann <mike.lehmann@gmail.com>
2017-10-19 09:41:53 -0700

    Update InventoryController.java

--- src/main/java/io/j4c/ecommerce/inventory/InventoryController.java
+++ src/main/java/io/j4c/ecommerce/inventory/InventoryController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. Change for CVS 
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. 
  */
 package io.j4c.ecommerce.inventory;
 
