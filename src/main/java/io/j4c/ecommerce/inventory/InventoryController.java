3b555e04dd97022f26f250b724806085ac27ab90
mikelehmann <mike.lehmann@gmail.com>
2017-10-19 12:00:58 -0700

    Update InventoryController.java

--- src/main/java/io/j4c/ecommerce/inventory/InventoryController.java
+++ src/main/java/io/j4c/ecommerce/inventory/InventoryController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. 
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. AMEX change
  */
 package io.j4c.ecommerce.inventory;
 
